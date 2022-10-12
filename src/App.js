87f7ad8622c1160553e2d8fa49887d838e2f5145
Duvan Perez <duvan.dev05@gmail.com>
2022-10-11 21:58:12 -0500

    Se crean los archivos de .css por cada componente, y a cada componente se le importa su respectivo archivo de .css, se modifica el html con el link de google fonts, dejado estilizada la aplicacion de TODOs

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const todos = [
   {text: 'Cortar cebolla', completed: false},
   {text: 'Tomar el curso de intro a React', completed: false},
   {text: 'Llorar con la llorona', completed: false},
+  { text: 'LALALALAA', completed: false },
 ]
 
 function App() {
--- src/CreateTodoButtom.js
+++ src/CreateTodoButtom.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './CreateTodoButtom.css';
 
 function CreateTodoButtom(){
     return(
-        <button>+</button>
+        <button className="CreateTodoButton">+</button>
     );
 }
 
