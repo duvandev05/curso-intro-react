43c39e90177d59127ab619d0643e26bcb676a0fb
Duvan Perez <duvan.dev05@gmail.com>
2022-10-23 17:55:35 -0500

    Se crea el archivo todocontext, para ordenar mejor toda l logica de la aplicacion para utilizar los provider y los concumer

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
 import { TodoCounter } from "../TodoCounter";
 import { TodoSearch } from "../TodoSearch";
 import { TodoList } from "../TodoList";
@@ -6,48 +7,40 @@ import { TodoItem } from "../TodoItem";
 import { CreateTodoButtom } from "../CreateTodoButtom";
 //import './App.css';
 
-function AppUI({
-  loading,
-  error,
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  completeTodo,
-  deleteTodo,
-}){
+function AppUI(){
     return(
         <React.Fragment>
-      <TodoCounter
-        total = {totalTodos}
-        completed = {completedTodos}
-      />
-      <TodoSearch
-        searchValue = {searchValue}
-        setSearchValue = {setSearchValue}
-      />
+          <TodoCounter/>
+          <TodoSearch/>
 
-      <TodoList>
-        {error && <p>Desespérate, hubo un error...</p>}
-        {loading && <p>Estamos cargando, no desesperes...</p>}
-        {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+          <TodoContext.Consumer>
+            {({error,
+              loading,
+              searchedTodos,
+              completeTodo,
+              deleteTodo,
+            }) => (
 
-        {searchedTodos.map(todo => (
-          <TodoItem 
-          key={todo.text}
-          text={todo.text}
-          completed={todo.completed}
-          onComplete={() => completeTodo(todo.text)}
-          onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
+                <TodoList>
+                  {error && <p>Desespérate, hubo un error...</p>}
+                  {loading && <p>Estamos cargando, no desesperes...</p>}
+                  {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+          
+                  {searchedTodos.map(todo => (
+                    <TodoItem 
+                    key={todo.text}
+                    text={todo.text}
+                    completed={todo.completed}
+                    onComplete={() => completeTodo(todo.text)}
+                    onDelete={() => deleteTodo(todo.text)}
+                    />
+                  ))}
+                </TodoList>
+            )}
+          </TodoContext.Consumer>
 
-      <CreateTodoButtom/>
-
-   </React.Fragment>
-
+          <CreateTodoButtom/>
+        </React.Fragment>
     )
 }
 
---
+++ src/TodoContext/useLocalStorage.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+/*funcion de custom react hook para el manejo del estado de localstorage */
+function useLocalStorage(itemName, initialValue) {
+    const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(true);
+    const [item, setItem] = React.useState(initialValue);
+  
+    React.useEffect(() => {
+      setTimeout(() => {
+        try {
+          const localStorageItem = localStorage.getItem(itemName);
+          let parsedItem;
+          
+          if (!localStorageItem) {
+            localStorage.setItem(itemName, JSON.stringify(initialValue));
+            parsedItem = initialValue;
+          } else {
+            parsedItem = JSON.parse(localStorageItem);
+          }
+  
+          setItem(parsedItem);
+          setLoading(false);
+        } catch(error) {
+        // En caso de un error lo guardamos en el estado
+          setError(error);
+        }
+      }, 1000);
+    },[])
+  
+    const saveItem = (newItem) => {
+     try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+     } catch (error) {
+      setError(error);
+     }
+    };
+  
+    return {
+      item,
+      saveItem,
+      loading,
+      error
+    };
+  }
+
+  
+export {useLocalStorage};  
